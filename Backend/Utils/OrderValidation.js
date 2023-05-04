398dc29be8ba818b5ef5b5b0faef2148313b640d
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-05-05 01:49:50 +0300

    order validation

--- Backend/Utils/OrderValidation.js
+++ Backend/Utils/OrderValidation.js
@@ -2,20 +2,29 @@ const Ajv = require("ajv");
 const ajv = new Ajv();
 
 const orderSchema = {
-    type: "object",
-    properties: {
-       
-        products:{
-            type: "array"
+  type: "object",
+  properties: {
+    products: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          product_name: { type: "string" },
+          product_image: { type: "string" },
+          price: { type: "number" },
+          quantity: { type: "number" },
+          product_details: { type: "string" },
         },
-        status: {
-            type: "string",
-            enum:['Pending', 'Accepted', 'Canceled']
-        }
+        required: ["product_name", "price", "quantity"],
+      },
     },
-    required: ["status"],
-    additionalProperties: false,
-}
+    status: {
+      type: "string",
+      enum: ["Pending", "Accepted", "Canceled"],
+    },
+  },
+  required: ["status"],
+  additionalProperties: false,
+};
 
-
-module.exports = ajv.compile(orderSchema)
+module.exports = ajv.compile(orderSchema);
