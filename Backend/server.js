03a2be44ee9f6f8a13296885c99bb91a84439053
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-05-07 21:14:46 +0300

    installing cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,9 @@ const path = require("path");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const cors = require('cors');
 
-
+app.use(cors());
 
 let UsersRoute = require("./Routes/UsersRouter");
 let LoginRoute = require("./Routes/LoginRoute");
