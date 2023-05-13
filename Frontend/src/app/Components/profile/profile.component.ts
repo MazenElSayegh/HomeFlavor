c880d86531e4cf2b5475ca294459adfb7eab0bff
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-05-13 22:00:57 +0300

    editing backend and editing css and profile component

--- Frontend/src/app/Components/profile/profile.component.ts
+++ Frontend/src/app/Components/profile/profile.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BackendService } from 'src/app/Services/backend.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  ID: any;
+  user: any;
+
+  constructor(public myRoute: ActivatedRoute,public myService: BackendService){
+    this.ID = myRoute.snapshot.params['id'];
+
+  }
+  ngOnInit(): void {
+    this.myService.getUserByID(this.ID).subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log(data);
+
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+
 
 }
