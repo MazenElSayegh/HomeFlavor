bd313a380c31f8104caef234f4882537b0568070
Ahmed Fahmi <amfahmi.96@gmail.com>
2023-05-17 05:06:24 +0300

    cart and local storage

--- Frontend/src/app/Components/header/header.component.ts
+++ Frontend/src/app/Components/header/header.component.ts
@@ -7,5 +7,52 @@ import { Component,Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  localStore:any;
   @Input() addedProducts:any;
+  // removeProduct(product:any){
+  //   this.addedProducts.splice(product)
+  // }
+  constructor() {
+    console.log(this.addedProducts)
+    if (this.addedProducts===undefined){
+    let data=localStorage.getItem("cart")
+    if(data){
+      this.addedProducts=JSON.parse(data)
+    }
+  }
 }
+
+  removeProduct(product:any){
+    let arrayOfProducts=localStorage.getItem("cart")
+    if(arrayOfProducts){
+      console.log("hiiiiii")
+    let data=JSON.parse(arrayOfProducts)
+    console.log(data)
+    data.forEach( (item:any, index:any) => {
+      console.log(item)
+      console.log(product)
+      if(item._id === product._id){
+        data.splice(index,1);
+        localStorage.setItem("cart",JSON.stringify(data))
+        this.addedProducts=data
+      }
+    });
+  }
+ }
+
+
+
+ ngOnInit(): void {
+  console.log(this.addedProducts)
+  if (this.addedProducts === undefined) {
+  let data=localStorage.getItem("cart")
+    if(data){
+
+      this.addedProducts = JSON.parse(data);
+    }
+  console.log(localStorage.getItem("cart"))
+  }
+}
+}
+
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -15,6 +15,30 @@ export class AppComponent {
     const child:StoreDetailsComponent=data;
     child.addedToCart.subscribe((data)=>{
       this.addedProducts.push(data);
+      console.log(data);
+      interface MyData {
+        price:number,
+        product_details:string,
+        product_image:string,
+        product_title:string,
+        store_id:string,
+        _id:string
+      }
+
+      const myDataString = localStorage.getItem('cart');
+      let myData: MyData[]
+
+      if (myDataString) {
+        myData = JSON.parse(myDataString);
+        myData.push(data)
+        localStorage.setItem("cart",JSON.stringify(myData))
+      }else{
+        myData=[]
+        myData.push(data)
+        localStorage.setItem("cart",JSON.stringify(myData))
+      }
+      console.log(localStorage.getItem('cart'))
+
     })
     }
   }
