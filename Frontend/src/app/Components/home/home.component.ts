28b4852845aa723cd4799591c928bbf32d6bddb8
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-05-25 21:39:26 +0300

    deleting logs and comments in orders part

--- Frontend/src/app/Components/home/home.component.ts
+++ Frontend/src/app/Components/home/home.component.ts
@@ -6,45 +6,47 @@ import { StoresService } from 'src/app/Services/stores.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  stores:any;
-  localhost="http://localhost:7005";
-  user_data:any;
-  storeFlag=1;
-  constructor(private storesService: StoresService,private localStorageService: LocalStorageService,private ordersService: OrdersService) {
+  stores: any;
+  localhost = 'http://localhost:7005';
+  user_data: any;
+  storeFlag = 1;
+  constructor(
+    private storesService: StoresService,
+    private localStorageService: LocalStorageService,
+    private ordersService: OrdersService
+  ) {
     storesService.getAllStores().subscribe({
       next: (data) => {
-        this.stores=data;
-        console.log(data);
+        this.stores = data;
       },
       error: (err) => {
         console.log(err);
       },
     });
-    if(this.user_data){
-    ordersService.GetAllOrders().subscribe({
-      next: (data)=>{
-        console.log(data);
-        if(data=="none"){
-          this.storeFlag=0;
-        }
-      },
-      error: (err)=>{
-        console.log(err);
-      },
-    });
-  }}
+    if (this.user_data) {
+      ordersService.GetAllOrders().subscribe({
+        next: (data) => {
+          if (data == 'none') {
+            this.storeFlag = 0;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
   ngOnInit(): void {
     this.localStorageService.getData('jwt_token').subscribe((data) => {
-     if(data){
-      this.user_data = data;}
+      if (data) {
+        this.user_data = data;
+      }
     });
-    console.log(this.user_data);
-
-}
-  onLinkClick(e:any){
-    e.preventDefault()
+  }
+  onLinkClick(e: any) {
+    e.preventDefault();
   }
 }
--- Frontend/src/app/Components/orders/orderdetails/orderdetails.component.ts
+++ Frontend/src/app/Components/orders/orderdetails/orderdetails.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 import { OrdersService } from 'src/app/Services/orders.service';
-import { ActivatedRoute,Router } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-orderdetails',
   templateUrl: './orderdetails.component.html',
-  styleUrls: ['./orderdetails.component.css']
+  styleUrls: ['./orderdetails.component.css'],
 })
 export class OrderdetailsComponent {
-  id:any;
-  order:any;
-  constructor(myRoute:ActivatedRoute, public myService: OrdersService, private router: Router){
-    this.id= myRoute.snapshot.params["id"];
+  id: any;
+  order: any;
+  constructor(
+    myRoute: ActivatedRoute,
+    public myService: OrdersService,
+    private router: Router
+  ) {
+    this.id = myRoute.snapshot.params['id'];
   }
   ngOnInit(): void {
-    this.myService.GetOrderByID(this.id).subscribe(
-      {
-        next:(data)=>{this.order=data;
-        console.log(this.order.user_id);},
-        error:(err)=>{console.log(err);
-        }
-      }
-    )
+    this.myService.GetOrderByID(this.id).subscribe({
+      next: (data) => {
+        this.order = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
