fd2ced8fefa7bc390ce50c450ec37cd66b9a13be
Omar Mohamed <omarmohamedradi97@gmail.com>
2023-05-26 00:16:23 +0300

    clear comment

--- Backend/Controllers/LogoutController.js
+++ Backend/Controllers/LogoutController.js
@@ -1,14 +1,6 @@
 let jwt = require("jsonwebtoken");
 var logoutUser = async (req, res) => {
   var token = req.headers.authorization?.split(" ")[1];
-  // jwt.verify(token, "token", (err, decoded) => {
-  //   if (err) {
-  //     return res.status(401).json({ message: "Invalid token" });
-  //   }
-  //   token = "";
-  //   res.json({ message: "Logged out successfully" });
-  // });
-
   jwt.sign(token, "", { expiresIn: 1 }, (logout, err) => {
     if (logout) {
       res.send({ msg: "You have been Logged Out" });
--- Frontend/src/app/Interceptors/auth.interceptor.ts
+++ Frontend/src/app/Interceptors/auth.interceptor.ts
@@ -30,18 +30,14 @@ export class AuthInterceptor implements HttpInterceptor {
     else {
       return next.handle(request).pipe(
         tap((event) => {
-          console.log(request);
           if (event instanceof HttpResponse) console.log(event.headers);
           if (
-            event instanceof HttpResponse && // Check if the event is an instance of the HttpResponse type
+            event instanceof HttpResponse &&
             event.headers.has('X-Auth-Token')
           ) {
-            console.log('hoooope');
             this.token = event.headers.get('X-Auth-Token')?.split(' ')[1];
-            // const payload = jwt_decode(this.token);
-            // console.log('JWT payload:', payload);
+
             localStorage.setItem('jwt_token', this.token);
-            // window.sessionStorage.setItem('jwt_token', this.token);
           }
         })
       );
