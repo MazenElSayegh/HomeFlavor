6cf4aac08145cb7f55a41355af5a50bcf9b23d2d
Ahmed Fahmi <amfahmi.96@gmail.com>
2023-06-21 19:29:45 +0300

    users with most orders

--- Frontend/src/app/Components/home/home.component.ts
+++ Frontend/src/app/Components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component,OnDestroy } from '@angular/core';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
 import { OrdersService } from 'src/app/Services/orders.service';
 import { StoresService } from 'src/app/Services/stores.service';
+import { RouteReuseStrategy } from '@angular/router';
+import { BackendService } from 'src/app/Services/backend.service';
 
 interface Store {
   _id: string;
@@ -10,6 +12,19 @@ interface Store {
  image: string;
   city: string;
   about: string;
+}
+
+interface User {
+
+_id:string,
+user_name:"string",
+email:"string",
+password:"string",
+user_image:"string",
+gender:"string",
+role:"string",
+mobile:"string",
+address:"string",
 }
 
 interface Order {
@@ -23,6 +38,7 @@ interface Order {
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+
 })
 export class HomeComponent implements OnDestroy {
   chartOptions = {
@@ -36,7 +52,20 @@ export class HomeComponent implements OnDestroy {
       ]
     }]
   };
+
+  ordersChartOptions = {
+    title: {
+      text: "Users with most orders"
+    },
+    data: [{
+      type: "column",
+      dataPoints: [
+        { label: "",  y: 0 },
+      ]
+    }]
+  };
   searchTerm:any;
+  users:any=[]
   stores: any=[]
   orders:any=[]
   displayedStores: any = this.stores;
@@ -44,12 +73,16 @@ export class HomeComponent implements OnDestroy {
   user_data: any;
   storeFlag = 1;
   timeout:any = null;
+  usersTimeout:any = null;
   chart: any;
+  usersChart:any;
   arrayCounts:any=[];
+  usersArrayCounts:any=[];
   constructor(
     private storesService: StoresService,
     private localStorageService: LocalStorageService,
-    private ordersService: OrdersService
+    private ordersService: OrdersService,
+    private backendService: BackendService
   ) {
 
     storesService.getAllStores().subscribe({
@@ -63,11 +96,19 @@ export class HomeComponent implements OnDestroy {
         console.log(err);
       },
     });
+
+    backendService.getAllUsers().subscribe({
+      next: (data) => {
+        this.users= data;
+        console.log(this.users)
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
     ordersService.GetAllOrders().subscribe({
       next: (data) => {
         this.orders=data;
-        console.log(this.orders)
-        console.log(this.stores)
         for (let i = 0; i < this.stores.length; i++) {
 
           let count =0
@@ -75,6 +116,16 @@ export class HomeComponent implements OnDestroy {
           const filteredArray = this.orders.filter((order:Order) => order.store_id && (order.store_id._id === this.stores[i]._id));
           count = filteredArray.length;
             this.arrayCounts.push({"name":this.stores[i].name,"count":count})
+        }
+
+        for (let i = 0; i < this.users.length; i++) {
+
+          let countUsers =0
+
+          const filteredUsersArray = this.orders.filter((order:Order) => order.user_id && (order.user_id._id === this.users[i]._id));
+          countUsers = filteredUsersArray.length;
+            this.usersArrayCounts.push({"name":this.users[i].user_name,"count":countUsers})
+            console.log("heere",this.usersArrayCounts)
         }
 
         if (data == 'none') {
@@ -112,6 +163,10 @@ export class HomeComponent implements OnDestroy {
     this.chart = chart;
     this.updateChart();
   }
+  ordersGetChartInstance(chart: object) {
+    this.usersChart = chart;
+    this.updateOrdersChart();
+  }
 
   ngOnDestroy() {
     clearTimeout(this.timeout);
@@ -128,5 +183,18 @@ export class HomeComponent implements OnDestroy {
     this.chart.render();
 
     this.timeout = setTimeout(this.updateChart, 1000);
+  };
+
+  updateOrdersChart = () => {
+    const sortedArray = this.usersArrayCounts.sort((a:any, b:any) => b.count - a.count);
+    this.usersArrayCounts=sortedArray
+    for (let i = 0; i < 5; i++) {
+        if(this.usersArrayCounts[i]){
+          this.ordersChartOptions.data[0].dataPoints[i]={label:this.usersArrayCounts[i].name, y:this.usersArrayCounts[i].count}
+        }
+    }
+    this.usersChart.render();
+
+    this.usersTimeout = setTimeout(this.updateOrdersChart, 1000);
   };
 }
